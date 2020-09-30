4785268d2e9c86b71c5c9c9f2960ce507f76d6d5
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-09-30 16:26:48 -0700

    sign in funtionality
    - allows uers to sign in or sign up
    - can also use google auth to sign in/up
    - if user signed in and access /signIn, it redirects them to home page
    - can sign in/out from avatar dropdown menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,11 +3,22 @@ import { useHistory, useLocation, matchPath, Link } from 'react-router-dom';
 import profilePic from '../images/user_icon.jpg';
 import './Nav.css';
 import searchIcon from '../images/search.svg';
+import { auth } from '../firebase';
+
 function Nav(){
     const [show, handleShow] = useState(false);
     const [dropdown, setDropdown] = useState(false);
+    const [isSignedIn, setIsSignedIn] = useState(!!auth.currentUser);
     let history = useHistory();
     let location = useLocation();
+    
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+          setIsSignedIn(true);
+        } else {
+          setIsSignedIn(false);
+        }
+      });
 
     function clickNav(){
         matchPath(location.pathname, {path:'/:id'}) ? history.push("/") : window.scrollTo(0,0);
@@ -40,7 +51,7 @@ function Nav(){
                 <Link to="/movies" onClick={toggleDropdown}>Movies</Link>
                 <Link to="/new" onClick={toggleDropdown}>New</Link>
                 <Link to="/mylist" onClick={toggleDropdown}>My List</Link>
-                <Link to="/" onClick={toggleDropdown}>Sign In</Link>
+                {!isSignedIn ? <Link to="/signIn" onClick={toggleDropdown}>Sign In</Link> : <span onClick={()=>{auth.signOut(); toggleDropdown()}}>Sign Out</span>}
             </div>
         </div>
     )
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,42 +1,79 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory, Redirect } from 'react-router-dom';
 import './SignIn.css';
-import { auth } from '../firebase';
+import { auth, googleAuth } from '../firebase';
 import googleIcon from '../images/google.svg';
 
 
 function LoginDialog(){
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [isSignedIn, setIsSignedIn] = useState(!!auth.currentUser);
+   
     const [error, setError] = useState('');
+    const [signUp, setSignUp] = useState(false);
+    let history = useHistory();
+
+    const [authState, setAuthState] = useState({
+        isSignedIn: false,
+        pending: true,
+        user: null,
+    });
+
+    useEffect(() => {
+        const unregisterAuthObserver = auth.onAuthStateChanged(user =>
+            setAuthState({ user, pending: false, isSignedIn: !!user })
+        )
+        return () => unregisterAuthObserver();
+    }, [])
+
 
     function login(){
         auth.signInWithEmailAndPassword(email, password)
-        .then(setIsSignedIn(true))
+        .then(() => history.push('/'))
         .catch(e => setError(e.message));
     }
-    
+    function signUser(){
+        auth.createUserWithEmailAndPassword(email, password)
+        .then(() => history.push('/'))
+        .catch( e => setError(e.message));
+    }
+    function toggleDialog(){
+        setSignUp(prevSignUp => !prevSignUp);
+    }
+    function googleSignIn(){
+        auth.signInWithPopup(googleAuth)
+        .then(()=> history.push('/'))
+        .catch(e => setError(e.message));
+    }   
+
+    if(authState.pending){
+        return <div className="bg"/>
+    }
+    if(!authState.pending && authState.isSignedIn){
+        return <Redirect to="/" />
+    }
+
     return(
         <div className="bg">
         <div className="container">
-        <h1 className="container-title">Sign In</h1>
-            {error !== null && <div>{error}</div>}
-            <div className="error">Error</div>
-            
+        <h1 className="container-title">{!signUp ? "Sign In" : "Sign Up"}</h1>
+            {error && <div className="error">{error}</div>}
+            {signUp && <input className="input" type="text" name="name" value={name} placeholder="Name" id="name" onChange={event => setName(event.target.value)} />}
             <input className="input" type="email" name="email" value={email} placeholder="Email" id="email" onChange={event => setEmail(event.target.value)} />
             <input className="input" type="password" name="userPassword" value={password} placeholder="Password" id="password" onChange={event => setPassword(event.target.value)} />
             
-            <button className="signin-btn" onClick={login}>Sign in</button>
-            <div className="forgot-container">
-                <Link className="forgot-password">Forgot password?</Link>
-            </div>
+            {!signUp ? <button className="signin-btn" onClick={login}>Sign In</button> : <button className="signin-btn" onClick={signUser}>Sign Up</button>}
+            {!signUp && <div className="forgot-container">
+                <Link className="forgot-password" to="/forgot-password">Forgot password?</Link>
+            </div>}
             <div className="google-container">
                 <img className="google" src={googleIcon} alt="google icon" />
-                <span className="google-text">Sign In with google</span>
+                <span className="google-text" onClick={googleSignIn}>Sign In with google</span>
             </div>
 
-            <p className="signup"> New to Nedflix? <Link to="/signUp" className="signup-link">Sign up now</Link></p>
+            {!signUp ? <p className="signup"> New to Nedflix? <span className="signup-link" onClick={toggleDialog}>Sign up now</span></p> : 
+            <p className="signup"> Aleady have an account? <span className="signup-link" onClick={toggleDialog}>Sign In</span></p> }
 
       </div>
       </div>
